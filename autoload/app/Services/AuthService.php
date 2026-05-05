4943de339b520c4d91fbfe96a55ff41c2d2e34c3
Daniel_ecc <dancord12072002@gmail.com>
2026-05-05 09:49:34 -0500

    Subida de proyecto

---
+++ autoload/app/Services/AuthService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class AuthService
+{
+    public function verificarEstado()
+    {
+        return "Autoload funcionando: Clase AuthService cargada exitosamente.";
+    }
+}
