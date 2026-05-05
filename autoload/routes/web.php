9f6fd3f4c73cada0633f28a5a4943af7c50566a9
Daniel_ecc <dancord12072002@gmail.com>
2026-05-05 10:08:09 -0500

    Adición de método

--- autoload/routes/web.php
+++ autoload/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Services\AuthService; // Sustituye a cualquier include_once
+
+Route::get('/prueba-autoload', function () {
+    $servicio = new AuthService(); // Instanciación automática
+    return $servicio->verificarEstado();
+});
 
 Route::get('/', function () {
     return view('welcome');
